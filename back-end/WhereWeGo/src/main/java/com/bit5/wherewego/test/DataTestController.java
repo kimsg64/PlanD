149109cfc93564139e51a8bc9d9dbaa207809c07
yvnalee <leeyvna@gmail.com>
2021-10-05 19:10:21 +0900

    commit

--- back-end/WhereWeGo/src/main/java/com/bit5/wherewego/test/DataTestController.java
+++ back-end/WhereWeGo/src/main/java/com/bit5/wherewego/test/DataTestController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
