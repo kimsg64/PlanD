5eb5a87b9aea7e6cf41753eb173bf76f26a08ea0
yvnalee <leeyvna@gmail.com>
2021-10-12 15:49:43 +0900

    course

--- back-end/WhereWeGo/src/main/java/com/bit5/wherewego/course/CourseVO.java
+++ back-end/WhereWeGo/src/main/java/com/bit5/wherewego/course/CourseVO.java
@@ -4,13 +4,9 @@ public class CourseVO {
 	private int c_num;
 	private String name;
 	private String userid;
-	private String time;
 	private int stcode;
-	private String info;
-	private String pcode1;
-	private String pcode2;
-	private String pcode3;
 	private int score;
+	private String stname; //역이름
 	
 	public int getC_num() {
 		return c_num;
@@ -30,47 +26,25 @@ public class CourseVO {
 	public void setUserid(String userid) {
 		this.userid = userid;
 	}
-	public String getTime() {
-		return time;
-	}
-	public void setTime(String time) {
-		this.time = time;
-	}
+	
 	public int getStcode() {
 		return stcode;
 	}
 	public void setStcode(int stcode) {
 		this.stcode = stcode;
 	}
-	public String getInfo() {
-		return info;
-	}
-	public void setInfo(String info) {
-		this.info = info;
-	}
-	public String getPcode1() {
-		return pcode1;
-	}
-	public void setPcode1(String pcode1) {
-		this.pcode1 = pcode1;
-	}
-	public String getPcode2() {
-		return pcode2;
-	}
-	public void setPcode2(String pcode2) {
-		this.pcode2 = pcode2;
-	}
-	public String getPcode3() {
-		return pcode3;
-	}
-	public void setPcode3(String pcode3) {
-		this.pcode3 = pcode3;
-	}
+	
 	public int getScore() {
 		return score;
 	}
 	public void setScore(int score) {
 		this.score = score;
+	}
+	public String getStname() {
+		return stname;
+	}
+	public void setStname(String stname) {
+		this.stname = stname;
 	}
 	
 	
