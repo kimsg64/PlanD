580fe65a6fdc628a3ce43406fa8efec525d17b0d
iamheesukim <hlogic15@gmail.com>
2021-10-19 15:40:44 +0900

    공지사항,예약 리스트 복구

--- back-end/WhereWeGo/src/main/java/com/bit5/wherewego/res/ResController.java
+++ back-end/WhereWeGo/src/main/java/com/bit5/wherewego/res/ResController.java
@@ -1,21 +1,10 @@
 package com.bit5.wherewego.res;
 
-import java.lang.System.Logger;
-import java.util.List;
-
-import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
 
 import com.bit5.wherewego.notice.PagingVO;
 
@@ -37,10 +26,10 @@ public class ResController {
 	public ModelAndView list(PagingVO pVo) {
 
 		ModelAndView mav = new ModelAndView();
-		ResDAOImp dao = sqlSession.getMapper(ResDAOImp.class);
-		ResDAOImp dao2 = sqlSession.getMapper(ResDAOImp.class);
 		
-		int total = dao2.totalRecordCount();
+		ResDAOImp dao = sqlSession.getMapper(ResDAOImp.class);
+		
+		int total = dao.totalRecordCount();
 		pVo.setTotalRecord(total);
 		
 		int num1 = pVo.getOnePageRecord() * pVo.getNowPage();
@@ -53,7 +42,8 @@ public class ResController {
 			num2 = pVo.getOnePageRecord();
 		}
 		
-		mav.addObject("list",dao.resAllSelect(num1,num2));
+		ResDAOImp dao2 = sqlSession.getMapper(ResDAOImp.class);
+		mav.addObject("list",dao2.resAllSelect(num1,num2));
 		mav.addObject("pVo",pVo);
 		mav.setViewName("res/resList");
 
