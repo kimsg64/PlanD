e4778d6d6d141139070ef522fee8df624b3d35d0
iamheesukim <hlogic15@gmail.com>
2021-10-20 12:59:01 +0900

    로그사용자 작성 코스 출력

--- back-end/WhereWeGo/src/main/java/com/bit5/wherewego/course/CourseDAOImp.java
+++ back-end/WhereWeGo/src/main/java/com/bit5/wherewego/course/CourseDAOImp.java
@@ -3,10 +3,10 @@ package com.bit5.wherewego.course;
 import java.util.List;
 
 import com.bit5.wherewego.notice.PagingVO;
-import com.bit5.wherewego.res.PlanningVO;
 
 public interface CourseDAOImp {
 	public List<CourseVO> courseAllSelect(int num1, int num2);
 	public int totalRecordCount(PagingVO pVo); //페이징
 	public int newCourseCount();
+	public List<CourseVO> myCourseSelect(String userid); //로그인한 사용자가 만든 코스 보기, 로그아이디값을 param으로 주시면됩니당
 }
