5dda68bc7bdb172b3ddf6319c209134b47fac5df
SeunggyuKim <mushon66@naver.com>
2021-10-25 12:29:42 +0900

    delete lin2

--- back-end/WhereWeGo/src/main/java/com/bit5/wherewego/course/CourseController.java
+++ back-end/WhereWeGo/src/main/java/com/bit5/wherewego/course/CourseController.java
@@ -3,7 +3,6 @@ package com.bit5.wherewego.course;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
